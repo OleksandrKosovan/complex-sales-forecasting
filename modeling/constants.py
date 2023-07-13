7aece63e40b1618010198a2f0faa6022a3c3c146
oleksa_kosovan <sanya.kosovan@gmail.com>
2023-07-13 15:52:42 +0300

    modeling PoC

--- modeling/constants.py
+++ modeling/constants.py
@@ -2,3 +2,10 @@ EXPERIMENT_NAME = "base"
 
 DATA_FOLDER = "modeling/data/"
 M5_FILE = "m5-dataset.parquet.gzip"
+
+HORIZON = 14
+N_WINDOWS = 10
+
+FULL_RESULTS_PATH = "modeling/results/full_cv/"
+EVALUATION_RESULTS_PATH = "modeling/results/evaluation/"
+METRICS_RESULTS_PATH = "modeling/results/metrics/"
